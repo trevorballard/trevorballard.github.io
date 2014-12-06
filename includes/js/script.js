de1a41888adbaff6176c6c6a32643b8c62c7f087
Trevor Ballard <trevorcoleballard@gmail.com>
2014-12-06 13:52:41 -0500

    Remove jQuery resize script

--- includes/js/script.js
+++ includes/js/script.js
@@ -17,18 +17,4 @@ $(function() {
 	if($(window).width() < 768) {
 		$('#carousel-col').removeClass('col-sm-12');
 	}
-	// Same as above, but works when resizing screen (otherwise need to refresh)
-	$(window).on('resize', function() {
-		var win = $(this); // this = window
-		
-		// If going from large to small
-		if($(win).width() < 992) {
-			$('#carousel-col').removeClass('col-sm-12');
-		}
-
-		// If going from small to large
-		if($(win).width() >= 768) {
-			$('#carousel-col').addClass('col-sm-12');
-		}
-	});
 });
